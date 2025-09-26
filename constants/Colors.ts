437d8302abe82d8758890a281efcefc60b290059
a-xel1 <133992614+a-xel1@users.noreply.github.com>
2025-09-26 17:28:16 +0000

    Add files via upload

---
+++ constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = '#0d3b66';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#000',
+    background: 'transparent',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
